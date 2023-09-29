07d842e62da3cd7ba98a6a447c83a12970b20c25
Priyesh Arya <122790187+PriyeshArya@users.noreply.github.com>
2023-09-29 20:06:55 +0530

    define "void helpApplication()" - Documentation
    To be used by "void help()"

---
+++ src/helpApplication.cpp
@@ -0,0 +1,58 @@
+#include <help.h>
+
+void helpApplication(){
+	system("cls");
+	
+	std::cout << std::endl;
+	std::cout << "          APPLICATION SPECIFIC          " << std::endl;
+	std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+	std::cout << std::endl;
+	
+	std::cout << "These commands are there to facilitate you interacting with the Application." << std::endl;
+	std::cout << "Let's Explore Each Command One-by-One." << std::endl;
+	std::cout << std::endl;
+	
+	std::cout << "1. CHANGE LOGIN KEY:" << std::endl;
+	std::cout << "--------------------" << std::endl;
+	std::cout << std::endl;
+	std::cout << " - To Change Application Login Key" << std::endl;
+	std::cout << std::endl;
+	std::cout << " - Example: /changekey" << std::endl;
+	std::cout << "            /setkey" << std::endl;
+	std::cout << std::endl;
+	system("pause");
+	
+	std::cout << std::endl;
+	std::cout << "2. EXIT APPLICATION:" << std::endl;
+	std::cout << "--------------------" << std::endl;
+	std::cout << std::endl;
+	std::cout << " - To Exit the Application; Same as ALT + F4." << std::endl;
+	std::cout << std::endl;
+	std::cout << " - Example: /exit" << std::endl;
+	std::cout << "            /close" << std::endl;
+	std::cout << std::endl;
+	system("pause");
+	
+	std::cout << std::endl;
+	std::cout << "3. CLEAR SCREEN:" << std::endl;
+	std::cout << "----------------" << std::endl;
+	std::cout << std::endl;
+	std::cout << " - To Clear the Screen" << std::endl;
+	std::cout << std::endl;
+	std::cout << " - Example: /clear" << std::endl;
+	std::cout << "            /cls" << std::endl;
+	std::cout << std::endl;
+	system("pause");
+	
+	std::cout << std::endl;
+	std::cout << "4. ABOUT:" << std::endl;
+	std::cout << "---------" << std::endl;
+	std::cout << std::endl;
+	std::cout << " - To know About the Application and Developer" << std::endl;
+	std::cout << std::endl;
+	std::cout << " - Example: /about" << std::endl;
+	std::cout << "            /info" << std::endl;
+	std::cout << std::endl;
+	system("pause");
+	system("cls");
+}
