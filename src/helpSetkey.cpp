f5a9a882cc71b3ff7d753c10a3023cb7ddf4f5ca
Priyesh Arya <122790187+PriyeshArya@users.noreply.github.com>
2023-09-29 20:11:11 +0530

    define "void helpSetkey()" - Documentation
    Instructions for changing Login Key/PIN.

---
+++ src/helpSetkey.cpp
@@ -0,0 +1,30 @@
+#include <help.h>
+
+void helpSetkey(){
+	system("cls");
+	std::cout << std::endl;
+	std::cout << "CHANGE LOGIN KEY:" << std::endl;
+	std::cout << "-----------------" << std::endl;
+	std::cout << std::endl;
+	std::cout << "To change/update Application Login Key." << std::endl;
+	std::cout << std::endl;
+	
+	std::cout << "COMMANDS: /setkey" << std::endl;
+	std::cout << "          /setpass" << std::endl;
+	std::cout << "          /changekey" << std::endl;
+	std::cout << "          /changekey" << std::endl;
+	std::cout << "          /updatekey" << std::endl;
+	std::cout << "          /updatepass" << std::endl;
+	std::cout << std::endl;
+	
+	std::cout << "PARAMETERS: none" << std::endl;
+	std::cout << std::endl;
+	
+	std::cout << "For Example:                 " << std::endl;
+	std::cout << "\t /setkey       - Change/Update Login Key" << std::endl;
+	std::cout << "\t /changekey    - Change/Update Login Key" << std::endl;
+	std::cout << std::endl;
+	
+	system("pause");
+	system("cls");
+}
