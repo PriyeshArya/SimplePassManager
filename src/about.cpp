fa93f7f1fb38c48460d2a8af34f6e05f3beeaa4a
Priyesh Arya <122790187+PriyeshArya@users.noreply.github.com>
2023-09-29 19:09:17 +0530

    define "void about()"

---
+++ src/about.cpp
@@ -0,0 +1,40 @@
+#include <utils.h>
+
+void about() {
+    system("cls");
+    std::cout << std::endl;
+
+    std::cout << "SIMPLE PASSWORD MANAGER - ABOUT" << std::endl;
+	std::cout << "===============================" << std::endl;
+	std::cout << std::endl;
+
+    std::cout << "Version: 1.1" << std::endl << std::endl;
+
+    std::cout << "Developer: Priyesh Arya" << std::endl;
+    std::cout << "GitHub: https://github.com/PriyeshArya" << std::endl;
+    std::cout << "LinkedIn: https://linkedin.com/in/PriyeshArya" << std::endl << std::endl;
+
+    std::cout << "DESCRIPTION:" << std::endl;
+    std::cout << "--------------------------" << std::endl;
+    std::cout << "The 'Simple Password Manager' is your trusty companion in the world of password management. " << std::endl;
+    std::cout << "It might not have all the bells and whistles of those fancy password managers, but it gets the job done...mostly." << std::endl << std::endl;
+
+    std::cout << "HOW IT WORKS:" << std::endl;
+    std::cout << "--------------------------" << std::endl;
+    std::cout << "- Passwords are encrypted using hexadecimal encryption." << std::endl;
+    std::cout << "- Data is stored in a CSV file because simplicity is key." << std::endl;
+    std::cout << "- An Application Login PIN provides an extra layer of security (well, sort of)." << std::endl;
+    std::cout << "- Bugs are a feature, not a flaw. We promise." << std::endl << std::endl;
+
+    std::cout << "DISCLAIMER:" << std::endl;
+    std::cout << "--------------------------" << std::endl;
+    std::cout << "This application is intended for educational purposes and should not be used for critical password management." << std::endl;
+    std::cout << "We accept no responsibility for lost or forgotten passwords." << std::endl << std::endl;
+
+    std::cout << "Have fun using our Simple Password Manager and remember, it's the journey that matters...or something like that." << std::endl;
+
+    std::cout << std::endl;
+    system("pause");
+    system("cls");
+}
+
